4e931dcd5704f0a0b6cbbfa88c3ebcd12e154999
wooju <woojukwon8@gmail.com>
2024-08-24 00:53:13 +0900

    remove debug label

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.orange),
       ),
